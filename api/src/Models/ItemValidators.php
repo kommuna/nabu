b8d9ed1935455ac50a1b7d0197e48c28f048858f
l80 <unit2013@hmamail.com>
2015-07-07 13:56:15 +0300

    PIX-124

--- api/src/Models/ItemValidators.php
+++ api/src/Models/ItemValidators.php
@@ -20,12 +20,16 @@ class ItemValidators {
             'promo_title' => v::oneOf(v::string(), v::nullValue()),
             'promo_url' => v::oneOf(v::string(), v::nullValue()),
             'tags' => v::oneOf(v::arr(), v::nullValue()),
-            'views_counter' => v::oneOf(v::int()->positive(), v::nullValue()),
+            'views_counter' => false,
             'posted_on' => v::oneOf(v::date(), v::nullValue()),
             'activated_on' => v::oneOf(v::date(), v::nullValue()),
             'is_param_1' => v::oneOf(v::bool(), v::nullValue()),
             'site' => v::oneOf(v::string(), v::nullValue()),
             'deleted_on' => false,
+            'favorites_counter' => false,
+            'votes_negative' => false,
+            'votes_positive' => false,
+            '_id' => false,
         ];
     }
 
