eac8c9c50af018f8900e096a607d7765da932b12
l80 <unit2013@hmamail.com>
2015-07-10 14:47:46 +0300

    PIX-124

--- api/src/Models/CategoryModel.php
+++ api/src/Models/CategoryModel.php
@@ -18,7 +18,7 @@ class CategoryModel extends Model {
             'name' => v::string()->length(1,256)->notEmpty(),
             'description' => v::string()->notEmpty(),
             'priority' => v::int()->notEmpty(),
-            'visible' => v::bool()->notEmpty()
+            'visible' => v::bool()
         ];
     }
 
